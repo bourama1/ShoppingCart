bf887facdb05d676f3146e919394d3f9b17597da
bennaito <benes.jakub1@gmail.com>
2020-10-16 15:37:18 +0200

    3. cvičení: Upravena inicializace třídy MainFrame. Práce na GUI. Přidána metoda pro přidání položky do košíku. Přidány komentáře, které pomohou s pochopením kódu.

--- src/uhk/fim/gui/MainFrame.java
+++ src/uhk/fim/gui/MainFrame.java
@@ -1,22 +1,79 @@
 package uhk.fim.gui;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class MainFrame extends JFrame {
+public class MainFrame extends JFrame implements ActionListener {
+    // Tlačítka deklarujeme zde, abychom k nim měli přístup v metodě actionPerformed
+    JButton btnInputAdd;
+    JButton btnInputRemove;
 
-    public MainFrame() {
-        initFrame();
+    public MainFrame(int width, int height) {
+        super("PRO2 - Shopping cart");
+        setSize(width, height);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
         initGUI();
     }
 
-    public void initFrame() {
-        setTitle("PRO2 - Shopping cart");
-        setSize(800, 600);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setVisible(true);
+    public void initGUI() {
+        // Vytvoříme hlavní panel, do kterého budeme přidávat další (pod)panely.
+        // Naším cílem při tvorbě GUI, je snaha jednotlivé komponenty zanořovat.
+        JPanel panelMain = new JPanel(new BorderLayout());
+
+        // Vytvoříme další 3 panely. Panel pro prvky formuláře pro přidání položky.
+        // Panel pro tabulku a panel pro patičku.
+        JPanel panelInputs = new JPanel(new FlowLayout(FlowLayout.LEFT)); // FlowLayout LEFT - komponenty chceme zarovnat zleva doprava.
+        JPanel panelTable = new JPanel();
+        JPanel panelFooter = new JPanel();
+
+        // Název
+        JLabel lblInputName = new JLabel("Název: ");
+        JTextField txtInputName = new JTextField("", 15);
+        // Cena za 1 kus
+        JLabel lblInputPricePerPiece = new JLabel("Cena/kus: ");
+        JTextField txtInputPricePerPiece = new JTextField("", 5);
+        // Počet kusů
+        JLabel lblInputPieces = new JLabel("Počet kusů: ");
+        JTextField txtInputPieces = new JTextField("", 5);
+
+        // Tlačítka
+        btnInputAdd = new JButton("Přidat");
+        btnInputAdd.addActionListener(this); // Nastavení ActionListeneru - kdo obslouží kliknutí na tlačítko.
+        btnInputRemove = new JButton("Smazat"); // Toto tlačítko zde máme jen pro ukázku - v budoucnu ho odebereme
+        btnInputRemove.addActionListener(this);
+
+        // Přidání komponent do horního panelu pro formulář na přidání položky
+        panelInputs.add(lblInputName);
+        panelInputs.add(txtInputName);
+        panelInputs.add(lblInputPricePerPiece);
+        panelInputs.add(txtInputPricePerPiece);
+        panelInputs.add(lblInputPieces);
+        panelInputs.add(txtInputPieces);
+        panelInputs.add(btnInputAdd);
+        panelInputs.add(btnInputRemove);
+
+        // Přidání (pod)panelů do panelu hlavního
+        panelMain.add(panelInputs, BorderLayout.NORTH);
+        panelMain.add(panelTable, BorderLayout.CENTER);
+        panelMain.add(panelFooter, BorderLayout.SOUTH);
+
+        // Přidání hlavního panelu do MainFrame (JFrame)
+        add(panelMain);
     }
 
-    public void initGUI() {
-
+    // Při kliknutí na jakékoliv tlačítko se zavolá tato metoda.
+    // Toho jsme docílili implementování rozhraní ActionListener a nastavením tlačítek např. btnInputAdd.addActionListener(this);
+    @Override
+    public void actionPerformed(ActionEvent actionEvent) {
+        // Metoda se volá pro každé tlačítko, musíme tedy rozhodnout, co se má skutečně stát pro konkrétní tlačítka
+        if(actionEvent.getSource() == btnInputAdd) {
+            // TODO: Domácí úkol
+            System.out.println("Přidán produkt: [NÁZEV], [CENA ZA KUS] Kč - [POČET KUSŮ] ");
+        } else if(actionEvent.getSource() == btnInputRemove) {
+            System.out.println("Kliknuto na tlačítko smazat!");
+        }
     }
 }
